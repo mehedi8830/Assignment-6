f872d097deeda06e9fc24f61a8dc8518a628572d
Mehedi Hasan <your-email@gmail.com>
2026-04-01 22:00:16 +0600

    add all item

--- eslint.config.js
+++ eslint.config.js
@@ -13,14 +13,7 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
